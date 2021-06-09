e4a1b6172d6c136bcfefffc8a71ec860c1898b4f
aymeric bonnaud <aymeric.bonnaud@edhec.com>
2021-06-09 16:55:28 +0200

    Finishing Rx & reviews app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Starting seeds..."
+
+puts "Destroy Restaurants"
+
+Restaurant.destroy_all
+
+puts "Restaurants destroyed !"
+
+puts "Creating Restaurants..."
+type = %w[chinese italian japanese french belgian]
+10.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name,
+                              category: type.sample,
+                              phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+                              address: Faker::Address.full_address)
+  restaurant.save
+end
+
+puts "#{Restaurant.count} Restaurants created"
